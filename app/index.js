bacb984bde4cd8ca3988081d6f1c8ac70853344d
Murat Gözel <murat@gozel.com.tr>
2021-03-04 00:45:59 +0300

    Updated app index.

--- app/index.js
+++ app/index.js
@@ -1,1 +1,9 @@
-console.log('This is FrondJS.');
+const {
+  init, externals, eventEmitter, render, config, asset, state, component, meta
+} = require('@frondjs/frond')
+
+config.set('env', window.process.env.NODE_ENV)
+config.set('name', window.frondjs.name)
+config.set('version', window.frondjs.version)
+
+console.log(`Project ${config.get('name')} ${config.get('version')} running in ${config.get('env')}`);
